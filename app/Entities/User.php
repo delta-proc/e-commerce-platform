3fbb19a118e885ff6d10906494050aa36f2048cb
Willem Leuverink <willem@mediacode.nl>
2019-10-18 14:59:28 +0200

    move User model namspace to App\Entities

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App;
+namespace App\Entities;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
